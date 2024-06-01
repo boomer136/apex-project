24e5f406851ad2a318d7cf61bbdf9c691fb4d139
boomer136 <calebmurphy136@gmail.com>
2024-06-01 08:01:34 -0500

    Created python script to execute task 2

---
+++ task2.py
@@ -0,0 +1,18 @@
+import hashlib
+import base64
+import json
+
+
+form_file_path = 'test_form.json'
+
+with open(form_file_path, 'r') as file:
+    form_contents = file.read()
+
+# Calculate the SHA-256 digest of the form contents 
+sha256_digest = hashlib.sha256(form_contents.encode('utf-8')).digest()
+
+# Base64 encode the SHA-256 digest 
+form_hash = base64.b64encode(sha256_digest).decode('utf-8')
+
+# Print the form hash
+print("Form Hash:", form_hash)
